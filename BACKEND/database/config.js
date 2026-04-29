a21ab640cf4a7a012cf35411f067be6cf2d75daf
EmilioQ21 <emilio.segura.v@outlook.com>
2026-04-28 20:27:11 -0600

    cambie la url de mongo a ver si asi si

--- BACKEND/database/config.js
+++ BACKEND/database/config.js
@@ -1,5 +1,5 @@
 
 module.exports = {
-    MONGODB_URI: "mongodb+srv://Poo:Guerrero_Dragon@crownestcrluster.fkfvpjh.mongodb.net/CrowNest"
+    MONGODB_URI: "mongodb://Poo:Guerrero_Dragon@ac-ff2lull-shard-00-00.fkfvpjh.mongodb.net:27017,ac-ff2lull-shard-00-01.fkfvpjh.mongodb.net:27017,ac-ff2lull-shard-00-02.fkfvpjh.mongodb.net:27017/CrowNest?ssl=true&replicaSet=atlas-fn4jel-shard-0&authSource=admin&appName=CrowNestCrluster"
 };
 

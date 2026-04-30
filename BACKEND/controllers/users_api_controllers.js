d9e03491deb9b8138afca60e6e528068c1f5d7f7
Omara Cámara <omara.camara@iteso.mx>
2026-04-30 10:13:47 -0600

    register forntend

--- BACKEND/controllers/users_api_controllers.js
+++ BACKEND/controllers/users_api_controllers.js
@@ -6,6 +6,7 @@ const jwt = require('jsonwebtoken');
 const secretKey = 'poo_el_guerrero_dragon';
 
 exports.registerUser = async (req, res) => { 
+     console.log("Se llamo a la funcion register del backend")
     try {
         if (req.body.password !== req.body.confirm_password) {
             return res.status(401).json ({
--- FRONTEND/controllers/users_controller.js
+++ FRONTEND/controllers/users_controller.js
@@ -1,26 +1,26 @@
-function register(){
+async function register(){
     event.preventDefault();
-    console.log("Se llamo a la funcion del register")
+    console.log("Se llamo a la funcion register del frontend")
     let data = new FormData(event.target);
 
-    fetch('/users', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(Object.fromEntries(data.entries()))
-    })
-    .then(response => {
-        if (!response.ok) alert(response.statusText);
-        return response.json();
-    })
-    .then(user => {
+    try{
+        const user = await fetch('/users', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(Object.fromEntries(data.entries()))
+        })
+        if (!user.ok) {
+            alert(user.statusText + "aqui ando");
+            return;
+        }
         sessionStorage.setItem('user', JSON.stringify(user));
-        window.location.href = local_url + 'home.html';
-    })
-    .catch(err => {
+        window.location.href = ENV.BACKEND_URL + 'home.html';
+    } catch(err){
         console.error('Error en register:', err);
-    });
+    }
+    
 }
 
 const registerForm = document.getElementById("registerForm");

e34d6a3a179d0f70e3a92850d6c8e86730fb46b2
EmilioQ21 <emilio.segura.v@outlook.com>
2026-04-30 08:09:25 -0600

    Le puse required se me olvido

--- BACKEND/models/list.js
+++ BACKEND/models/list.js
@@ -2,21 +2,21 @@
 const mogoose = require('mongoose');   
 
 const listSchema = {
-    id:          "number",   
-    userId:      "number",   
-    nombre:      "string",  
-    descripcion: "string",  
-    visibilidad: "string",  
+    id:          { type: "number",  required: true },
+    userId:      { type: "number",  required: true },
+    nombre:      { type: "string",  required: [true, "La lista debe tener nombre"] },
+    descripcion: { type: "string",  required: false },
+    visibilidad: { type: "string",  required: [true, "Debes asignar visibilidad"] },
     peliculas: [
         {
-            tmdbId:     "number", 
-            titulo:     "string",
-            poster_path:"string", 
-            agregadaEn: "date"    
+            tmdbId:      { type: "number", required: true },
+            titulo:      { type: "string", required: true },
+            poster_path: { type: "string", required: true },
+            agregadaEn:  { type: "date",   required: true }
         }
     ],
-    creadaEn:    "date",
-    actualizadaEn: "date"
+    creadaEn:      { type: "date", required: true },
+    actualizadaEn: { type: "date", required: true }
 };
 
 module.exports = mogoose.model('List', listSchema);

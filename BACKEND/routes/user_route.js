09d886d34cd91e6ce15bc212a65e4f52fb8f9dd6
Omara Cámara <OMARACAMARITA@gmail.com>
2026-05-10 14:39:16 -0600

    regreso a las rutas

--- BACKEND/routes/user_route.js
+++ BACKEND/routes/user_route.js
@@ -1,4 +1,4 @@
-/*
+
 const usersControllers = require( '../controllers/users_api_controllers.js');
 
 const express = require('express'); 
@@ -25,29 +25,3 @@ routerUsers.put('/:id',usersControllers.updateUserInfo);
 routerUsers.delete('/:id',usersControllers.deleteUserInfo);
 
 module.exports = routerUsers;   
-*/
-const usersControllers = require('../controllers/users_api_controllers.js');
-const express = require('express'); 
-const routerUsers = express.Router();
-
-// 1. Rutas Públicas (Sin auth)
-routerUsers.post('/', usersControllers.registerUser);
-
-// 2. Middleware de autenticación para TODO lo que sigue
-// Esto simplifica el código y evita repetirlo en cada línea
-routerUsers.use('/:id', usersControllers.authMiddelwere);
-
-// 3. Rutas Específicas de Acción (Métodos claros)
-routerUsers.delete('/:id', usersControllers.deleteUserInfo); // <-- Súbelo aquí
-routerUsers.put('/:id', usersControllers.updateUserInfo);
-routerUsers.get('/:id', usersControllers.getUser);
-
-// 4. Rutas con parámetros variables (Al final)
-routerUsers.get("/other/:id", usersControllers.getOtherUser);
-routerUsers.post('/add-friend/:id', usersControllers.addFriend);
-routerUsers.post('/friend-request/:requestId', usersControllers.handleFriendRequest);
-
-// Esta ruta es la más peligrosa por ser la más genérica, déjala al último
-routerUsers.get('/:type/:id', usersControllers.getUsers);
-
-module.exports = routerUsers;
